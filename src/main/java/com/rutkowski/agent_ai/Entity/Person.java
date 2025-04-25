97442b257a7e4cfad85d0a9282c1e53f91edbc49
Gucio <asd@asd.pl>
2025-04-25 14:04:31 +0200

    16 added Long Id column to Person enity

--- src/main/java/com/rutkowski/agent_ai/Entity/Person.java
+++ src/main/java/com/rutkowski/agent_ai/Entity/Person.java
@@ -1,15 +1,14 @@
 package com.rutkowski.agent_ai.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class Person {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String imie;
     private String nazwisko;
     private int wiek;
